eeac67921439380e5634379a9dc4cbfca7c2ce82
Knox2026 <keithrunion@gmail.com>
2026-03-25 06:16:41 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,35 @@
-self.addEventListener('install', (e) => {
+const CACHE_NAME = 'acaan-v8.6'; // Update this number whenever you push to GitHub
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      // Pre-cache the main files
+      return cache.addAll(['index.html', 'manifest.json']);
+    })
+  );
   self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            // Delete old versions immediately
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
 });
